55c429b8471933601146753fdfd457e9f864f790
Moshe Haiem <mhaiem9@gmail.com>
2020-12-19 18:56:28 -0800

    mh added a few comments and output accuracy

--- bin_clas.py
+++ bin_clas.py
@@ -5,10 +5,13 @@ from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 import math
 
+#calculates sigmoid functiom
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+
 class BinaryClassifer():
+    # init weight and training values
     weight = np.random.rand(1,31)
     global train_target
     global train_data
@@ -17,11 +20,7 @@ class BinaryClassifer():
 
 
     def __init__(self, train_data, train_target):
-        """Data is already loaded for you using sklearn.datasets.load_breast_cancer
-        utility function in auto-testing part. train_data is your training feature 
-        data and train_target is your train label data. Hint: you can store both 
-        training features and target vector as class variables"""
-        
+        #here, the x and y values are initialized. We also center the values around the mean 
         bias = np.ones((512,1))
         self.y = np.asarray(train_target.values)
         self.x = np.asarray(train_data.values)
@@ -35,18 +34,7 @@ class BinaryClassifer():
 
 
     def logistic_training(self, alpha, lam, nepoch, epsilon):
-        """Training process of logistic regression will happen here. User will provide
-        learning rate alpha, regularization term lam, specific number of training epoches,
-        and a variable epsilon to specify pre-mature end condition,
-        i.e., if error < epsilon, training stops.
-        The specification of these parameters is the same as that in program #1.
-        You implementation must include 3-fold validation,
-        but you are allowed to hardcode the minibatch size in your program.
-        Hint: You can store both weight and bias as class variables,
-        so other functions can directly use them"""
-        
-
-
+	# we want to initialize x and y as well as alpha array and weight array for n fold validation
         x = np.array_split(self.x, 3, axis=0)
         y = np.array_split(self.y, 3)
         alphaarr=np.zeros(3)
@@ -55,12 +43,15 @@ class BinaryClassifer():
         weightarr.append(np.zeros((1,31)))
         weightarr.append(np.zeros((1,31)))
         weightarr.append(np.zeros((1,31)))
-
+	
+	#Here we init an error array for wach n fold
         errorval = []
         errorval.append(float('inf'))
         errorval.append(float('inf'))
         errorval.append(float('inf'))
         errorarr = np.zeros(3)
+	
+	#each n is an iteration of cross validation. We set the x, y, and validation x and y for each iteration
         for n in range(3):
             tempx = 0
             tempy = 0
@@ -83,7 +74,7 @@ class BinaryClassifer():
                 valy = y[0]
 
 
-
+	    #here we set the parameters for 2d grid search.
             a = 0
             l = 0
             error = float('inf')
@@ -95,9 +86,9 @@ class BinaryClassifer():
             lam1 = int(lam[0])
             lam2 = int(lam[1])
             SGDIters = len(tempx)
+            batch_size = 4
 
-            #do n fold cross validation
-
+	    #2d grid search starts here
             for l in np.arange(lam1, lam2, (lam2-lam1) /step_size):
                 for a in np.arange(alpha1, alpha2, (alpha2-alpha1)/step_size):
                     cur_iter = 1
@@ -107,16 +98,17 @@ class BinaryClassifer():
                         prediction = np.zeros((1, 31))
                         for j in range(SGDIters):  
                             #here we implement batch gradient descent, and implement the correct gradient step
-                            # prediction += (-((self.y[j] - (1/(1 +np.exp((-np.dot(self.x[j],np.transpose(curr_weight))))))) * self.x[j]))
                             prediction += (-((tempy[j] - sigmoid((np.dot(tempx[j],np.transpose(curr_weight))))) * tempx[j]))
-                            if (((j+1) % 4) == 0):
+                            if (((j+1) % batch_size) == 0):
                                 w_gradient = (prediction) + (l*curr_weight)
                                 curr_weight += -(a*w_gradient)
                                 prediction = np.zeros((1,31))
                         cur_iter += 1
+		    #regularization
                     a /= (cur_iter)**.5
 
-
+		    
+		    # here, we calculate cross_entropy error
                     temp = 0
                     xn=0
                     entropyiters = len(valx)
@@ -158,7 +150,7 @@ class BinaryClassifer():
 
 
 
-
+	# we take the values from cross validation to do one more run of training
         optimalalpha = 0
         optimallambda = 0
         optimalweight = np.zeros((1,31))
@@ -219,11 +211,8 @@ class BinaryClassifer():
 
 
     def logistic_testing(self, testX):
-        """TestX is a numpy array, which is pre-determined in this hw, referring to
-        test_data in auto-testing part. (Friendly remainder: test_data is a panda dataframe)
-        Use your trained weight and bias to compute the predicted y values,
-        Predicted y values should be 0 or 1. return the numpy array in shape n*1"""
         xTest = testX.copy()
+        xTest = np.delete(xTest, 30, 1)
         meanval = xTest.mean(axis=0)
         xTest-= meanval
         maxi = np.max(abs(xTest), axis=0)
@@ -234,7 +223,6 @@ class BinaryClassifer():
         resultVec = np.zeros(57)
         for j in range(57):
             resultVec[j] = sigmoid((np.dot(np.transpose(weight),(xTest[j])) + bias))
-        counter = 0
         for i in range(57):
             if resultVec[i] >= .5:
                 resultVec[i] = 1
@@ -245,14 +233,6 @@ class BinaryClassifer():
 
 
     def svm_training(self, gamma, C):
-        """Training process of the support vector machine will happen here. We will always use Gaussian (rbf) kernel,
-        and the grid search parameters are gamma and C, which are both python lists with min and max range values.
-        You are allowed to use build-in grid search for SVM. Consulting documentation for specific usage.
-        As softmargin SVM can handle nonlinear boundaries and outliers much better than simple logistic regression,
-        we do not perform 3-fold validation here (just one training run with 90-10 training-validation split).
-        Furthmore, you are allowed to use SVM's built-in grid search method.
-        This function will be a "wrapper" around sklearn.svm.SVC with all other parameters take the default values.
-        Please consult sklearn.svm.SVC documents to see how to use its "fit" and "predict" functions. """
 
         #grid search params are gamma and C (lists with min and max)
         # built in grid search for SVM
@@ -268,59 +248,41 @@ class BinaryClassifer():
 
     def svm_testing(self, testX):
         """Use your trained SVM to return the numpy array in shape n*1, predicted y values should be 0 or 1"""
-        return self.clf.predict(testX).reshape(57,1)
-
-
-# """ Training Process: You only need to modify nepoch, epsilon of logistic training method.
-# Please donot modify anything for SVM training function. Please don't add your own svm_testing
-# or logistic_testing function when you submitting this HW. This is for auto-testing """
-# dataset = load_breast_cancer(as_frame=True)
-# """Dataset is divided into 90% and 10%, 90% for you to perform k-fold validation and 10% for
-# auto-tester to validate your performance. Please Donot change random_state, which will generate
-# difffernt partitions. We want to ensure fair competition among all students"""
-# train_data = dataset['data'].sample(frac=0.9, random_state=0) # random state is a seed value
-# train_target = dataset['target'].sample(frac=0.9, random_state=0) # random state is a seed value
-# test_data = dataset['data'].drop(train_data.index)
-# test_target = dataset['target'].drop(train_target.index)
-
-# model = BinaryClassifer(train_data, train_target)
-# """Only performance of logistic regression will be used for competition. However, you must implement SVM training
-# and Testing, generates reasonable results, above 90%"""
-# # Compute the time to do grid search on training logistic
-# logistic_start = time.time()
-# model.logistic_training([10**-10, 10], [10e-10, 1e10], 300, .0005)
-# logistic_end = time.time()
-# model.logistic_testing(test_data)
-# # Compute the time to do grid search on training SVM
-# svm_start = time.time()
-# model.svm_training([1e-9, 1000], [0.01, 1e10])
-# model.svm_testing(test_data)
-# svm_end = time.time()
+        xTest = testX.copy()
+        xTest = np.delete(xTest, 30, 1)
+        return self.clf.predict(xTest).reshape(57,1)
 
 
 
-
-""" Training Process: You only need to modify nepoch, epsilon of logistic training method.
-Please donot modify anything for SVM training function. Please don't add your own svm_testing
-or logistic_testing function when you submitting this HW. This is for auto-testing """
 dataset = load_breast_cancer(as_frame=True)
-"""Dataset is divided into 90% and 10%, 90% for you to perform k-fold validation and 10% for
-auto-tester to validate your performance. Please Donot change random_state, which will generate
-difffernt partitions. We want to ensure fair competition among all students"""
 train_data = dataset['data'].sample(frac=0.9, random_state=0) # random state is a seed value
 train_target = dataset['target'].sample(frac=0.9, random_state=0) # random state is a seed value
 test_data = dataset['data'].drop(train_data.index)
 test_target = dataset['target'].drop(train_target.index)
 
 model = BinaryClassifer(train_data, train_target)
-"""Only performance of logistic regression will be used for competition. However, you must implement SVM training
-and Testing, generates reasonable results, above 90%"""
 # Compute the time to do grid search on training logistic
 logistic_start = time.time()
-model.logistic_training([10**-10, 10], [10e-10, 1e10], 300, .0005)
+bias = np.ones((57,1))
+x = np.hstack((test_data.values, bias))
+log_train = model.logistic_training([10**-10, 10], [10e-10, 1e10], 300, .0005)
+log_test = model.logistic_testing(x)
 logistic_end = time.time()
 # Compute the time to do grid search on training SVM
 svm_start = time.time()
-model.svm_training([1e-9, 1000], [0.01, 1e10])
-svm_end = time.time()
+svm_train = model.svm_training([1e-9, 1000], [0.01, 1e10])
+svm_test = model.svm_testing(x)
 
+# compare accuracy of logistic testing vs sklearn built in SVM
+counter = 0
+for i in range(len(log_test)):
+	if log_test[i] == test_target.values[i]:
+		counter += 1
+print("logistic accuracy: ", counter / len(log_test))
+
+counter = 0
+for i in range(len(svm_test)):
+	if svm_test[i] == test_target.values[i]:
+		counter += 1
+print("svm accuracy: ", counter / len(svm_test))
+svm_end = time.time()
